4db9ff3d45d073613939e5ab3e77323e7e8850fa
Tuntun Kumar <tuntun@hrgdev.com>
2021-03-27 18:09:17 +0530

    Added router, bootstrap and all components for forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router'
+import { HighlightModule } from 'ngx-highlightjs';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsComponent } from './forms/forms.component';
+import { TemplateComponent } from './forms/template/template.component';
+import { ReactiveComponent } from './forms/reactive/reactive.component';
+import { NestedComponent } from './forms/nested/nested.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'template', pathMatch: 'full' },
+  { path: 'template', component: TemplateComponent },
+  { path: 'reactive', component: ReactiveComponent },
+  { path: 'nested', component: NestedComponent },
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    FormsComponent,
+    TemplateComponent,
+    ReactiveComponent,
+    NestedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HighlightModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/forms/nested/nested.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nested',
+  templateUrl: './nested.component.html',
+  styleUrls: ['./nested.component.scss']
+})
+export class NestedComponent implements OnInit {
+  states: Array<String> = ['AR', 'AL', 'CA', 'DC'];
+  fruits: Array<String> = ['Mango', 'Grapes', 'Strawberry', 'Oranges'];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
